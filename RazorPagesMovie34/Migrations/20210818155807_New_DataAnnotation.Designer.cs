3c9aa7cfc3f58fe8bfdefa98420adfc0d2ec2285
Scot <bscot.hagen@gmail.com>
2021-08-18 08:58:32 -0700

    Added migration for the Data annotations

--- RazorPagesMovie34/Migrations/20210818155807_New_DataAnnotation.Designer.cs
+++ RazorPagesMovie34/Migrations/20210818155807_New_DataAnnotation.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie34.Data;
 
 namespace RazorPagesMovie34.Migrations
 {
     [DbContext(typeof(RazorPagesMovie34Context))]
-    partial class RazorPagesMovie34ContextModelSnapshot : ModelSnapshot
+    [Migration("20210818155807_New_DataAnnotation")]
+    partial class New_DataAnnotation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
